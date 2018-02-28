8ae3da03fb7f8647a01b2788b8a0874b8406f3d5
Eric <wanghaitao93@163.com>
2018-02-28 08:42:10 +0800

    hook Clock updateClock method

--- app/src/main/java/com/example/eric/myapplication/Tutorial.java
+++ app/src/main/java/com/example/eric/myapplication/Tutorial.java
@@ -1,8 +1,12 @@
 package com.example.eric.myapplication;
 
+import android.graphics.Color;
+import android.widget.TextView;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import de.robv.android.xposed.XposedBridge;
 
 /**
  * Created by eric on 26/02/2018.
@@ -12,6 +16,20 @@ public class Tutorial implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+        if (!lpparam.packageName.equals("com.android.systemui"))
+            return;
+
+        //XposedBridge.log("Loaded app: " + lpparam.packageName);
+
+
+        XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                tv.setText(text + " :)");
+                tv.setTextColor(Color.GREEN);
+            }
+        });
     }
 }
